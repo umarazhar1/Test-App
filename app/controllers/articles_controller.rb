7cd4b7579a35bf0bf5de75311079051b64ff393a
Umar Azhar <148058627+umarazhar1@users.noreply.github.com>
2023-11-30 17:59:09 +0500

    All CRUD operations completed but without frontend styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
     #binding.break # Call to debugger
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -9,15 +10,48 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
     # render plain: params[:article] This line is not working in rails 7
-    @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to article_path(@article)
-    #redirect_to @article   # this line has the same effect as above line
-
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to article_path(@article)
+      #redirect_to @article   # this line has the same effect as above line
+    else
+      flash[:notice] = "Failed to create article!"
+      render 'new', status: :unprocessable_entity
+    end
   end
+  
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully."
+      redirect_to article_path(@article)
+    else
+      flash[:notice] = "Failed to update the article!"
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+  
 end
